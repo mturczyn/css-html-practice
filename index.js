68bd7b605756035eca9f612a030fe3353a4f575d
Michal Turczyn <turek1992@o2.pl>
2023-11-02 15:45:56 +0100

    First upload of all notes.

---
+++ index.js
@@ -0,0 +1,29 @@
+let INDEX_PAGE = ''
+
+function goTo(path) {
+    window.location = path
+}
+
+function createElementAndAppend(tag, text, id, scrollToElement) {
+    const container = document.getElementById(id)
+    const newElement = createElement(tag, text)
+    container?.appendChild(newElement)
+    scrollToElement && newElement.scrollIntoView()
+}
+
+function createElement(tag, text) {
+    const newElement = document.createElement(tag)
+    const innerText = document.createTextNode(text)
+    newElement.appendChild(innerText)
+    return newElement
+}
+
+function appendListItem(listId, scrollToElement) {
+    const itemCount = document.getElementById(listId).children.length
+    createElementAndAppend(
+        'li',
+        `item ${itemCount + 1}`,
+        listId,
+        scrollToElement
+    )
+}
