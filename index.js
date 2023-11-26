a9583ead234517c79794c844f728a620060221ec
Michal Turczyn <turek1992@o2.pl>
2023-11-26 22:01:39 +0100

    Improved navigation code, added notes about flexbox and more properties helping with flex container items alignment.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-let INDEX_PAGE = ''
+const FLEXBOX = '/layout/flexbox/'
+const LAYOUT = '/layout/'
+const MAIN_PAGE = '/'
+const COLORS = '/colors/'
+const INDEX = 'index.html'
+const BASE_DIR = 'C:/Users/MT/source/repos/css-practice'
 
 function goTo(path) {
     window.location = path
+}
+
+function getPagePath(whereTo) {
+    return `${BASE_DIR}${whereTo}${INDEX}`
+}
+
+function goToPage(pageDir) {
+    let pagePath = getPagePath(pageDir)
+    let targetPath = getRelativePath(window.location.href, pagePath)
+    goTo(targetPath)
+}
+
+function goToFlexBox() {
+    goToPage(FLEXBOX)
+}
+
+function goToLayout() {
+    goToPage(LAYOUT)
+}
+
+function goToMainPage() {
+    goToPage(MAIN_PAGE)
+}
+
+function goToColors() {
+    goToPage(COLORS)
 }
 
 function createElementAndAppend(tag, text, element, scrollToElement) {
@@ -32,4 +63,44 @@ function appendListItem(listId, scrollToElement) {
         listId,
         scrollToElement
     )
+}
+
+/**
+ * Given a source directory and a target filename, return the relative
+ * file path from source to target.
+ * @param source {String} directory path to start from for traversal
+ * @param target {String} directory path and filename to seek from source
+ * @return Relative path (e.g. "../../style.css") as {String}
+ */
+function getRelativePath(source, target) {
+    source = source.startsWith('file:///')
+        ? source.replace('file:///', '')
+        : source
+
+    var sep = source.indexOf('/') !== -1 ? '/' : '\\',
+        targetArr = target.split(sep),
+        sourceArr = source.split(sep),
+        filename = targetArr.pop(),
+        targetPath = targetArr.join(sep),
+        relativePath = ''
+
+    // If source path was given with file at the end, we want to remove it.
+    let lastItem = sourceArr[sourceArr.length - 1]
+    if (lastItem && lastItem.includes('.')) {
+        sourceArr.pop()
+    }
+
+    while (targetPath.indexOf(sourceArr.join(sep)) === -1) {
+        sourceArr.pop()
+        relativePath += '..' + sep
+    }
+
+    var relPathArr = targetArr.slice(sourceArr.length)
+    relPathArr.length && (relativePath += relPathArr.join(sep) + sep)
+
+    return relativePath + filename
+}
+
+function getRelativePathToRoot(target) {
+    return getRelativePath(BASE_DIR, target)
 }
