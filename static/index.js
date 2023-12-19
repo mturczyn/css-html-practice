76da3e3c990f8064f48895dc0021fc0fdd8581f4
Michal Turczyn <turek1992@o2.pl>
2023-12-19 15:12:22 +0100

    Added notes about gradients.

--- static/index.js
+++ static/index.js
@@ -22,6 +22,7 @@ const SHADOWS = 'shadows/'
 const SELECTORS_AND_CASCADE = 'selectors-and-cascade/'
 const FOCUS = 'focus/'
 const FUNCTIONS = 'functions/'
+const GRADIENTS = 'gradients/'
 const Z_INDEX = 'z-index/'
 
 function addEventListenerToElement(elementId, eventName, callback) {
@@ -48,6 +49,10 @@ function goToPage(pageDir) {
     let pagePath = getPagePath(pageDir)
     let targetPath = getRelativePath(window.location.href, pagePath)
     goTo(targetPath)
+}
+
+function goToGradients() {
+    goToPage(GRADIENTS)
 }
 
 function goToFunctions() {
