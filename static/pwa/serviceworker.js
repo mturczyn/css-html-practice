a59d19f14e31f1f70eac1bf14e69d8b59187999f
Michal Turczyn <turek1992@o2.pl>
2024-02-16 14:37:21 +0100

    Added implementation of service worker and using Cache Storage API. Added notes about all of these.

--- static/pwa/serviceworker.js
+++ static/pwa/serviceworker.js
@@ -8,15 +8,7 @@ self.addEventListener('activate', (event) => {
     console.log('Service worker activated.')
 })
 
-self.addEventListener('fetch', (event) => {
-    const requestUrl = event.request.url
-    console.log(`URL requested: ${requestUrl}`)
-
-    if (!requestUrl.endsWith('not-existing.css')) {
-        fetch(requestUrl)
-        return
-    }
-
+function produceCssResponse(forCache) {
     // We can multiple handlers for this event,
     // but execution chain will be terminated once
     // event.respondWith method is called.
@@ -26,14 +18,55 @@ self.addEventListener('fetch', (event) => {
             'Content-type': 'text/css',
         },
     }
-    const htmlResponse = new Response(
-        '#from-not-existing-styles { color: purple; font-weight: bolder; background: pink }',
-        options
-    )
+
+    let styles =
+        '#from-not-existing-styles { color: purple; font-weight: bolder; background: pink }'
+
+    if (forCache) {
+        styles +=
+            ' #from-not-existing-styles-header::before { content: "SERVED FROM CACHE!"}'
+    }
+
+    const cssResponse = new Response(styles, options)
+
+    return cssResponse
+}
+
+async function addNonExistingCssToCache(requestUrl) {
+    console.log('>>> Setting cache for non-existing response.')
+    cache = await caches.open('pwa-practice-assets')
+    // We need to create separate object, as response object used
+    // to produce return value (as HTTP response for the request)
+    // the object MUST NOT be used in other places, such as in cache.
+    await cache.put(requestUrl, produceCssResponse(true))
+    console.log('>>> Added cache for ' + requestUrl)
+}
+
+async function produceNonExistingCss(requestUrl) {
+    let cachedResponse = await caches.match(requestUrl)
+    if (cachedResponse) {
+        console.log('>>> Cache found for ' + requestUrl)
+        return cachedResponse
+    }
+    await addNonExistingCssToCache(requestUrl)
     // We can also create simple, plain text responses.
     // const simpleResponse = new Response('Body of the HTTP response')
+    return produceCssResponse(false)
+}
 
-    event.respondWith(htmlResponse)
+async function produceResponseWithCustomCssAndCache(requestUrl) {
+    console.log(`URL requested: ${requestUrl}`)
+
+    if (requestUrl.endsWith('not-existing.css')) {
+        return await produceNonExistingCss(requestUrl)
+    }
+
+    return await fetch(requestUrl)
+}
+
+// RespondWith must be called synchronously, but it accepts Promise resolving to Response object.
+self.addEventListener('fetch', (event) => {
+    event.respondWith(produceResponseWithCustomCssAndCache(event.request.url))
 })
 
 caches.open('pwa-practice-assets').then(async (cache) => {
